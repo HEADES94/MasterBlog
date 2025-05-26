42733f644f637b3f0b5a28b82f80ce12390445ea
joshui <joshuapaoletti@googlemail.com>
2025-05-26 17:16:07 +0200

    add functionality to create new blog posts with a dedicated form

--- app.py
+++ app.py
@@ -1,18 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 import os
 
 app = Flask(__name__)
+DATA_FILE = 'posts.json'
 
-# Hilfsfunktion zum Laden der Posts
 def load_posts():
-    with open('posts.json', 'r') as file:
+    if not os.path.exists(DATA_FILE):
+        return []
+    with open(DATA_FILE, 'r') as file:
         return json.load(file)
+
+def save_posts(posts):
+    with open(DATA_FILE, 'w') as file:
+        json.dump(posts, file, indent=4)
 
 @app.route('/')
 def index():
     blog_posts = load_posts()
     return render_template('index.html', posts=blog_posts)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        posts = load_posts()
+        new_post = {
+            "id": max([post["id"] for post in posts], default=0) + 1,
+            "author": request.form.get("author"),
+            "title": request.form.get("title"),
+            "content": request.form.get("content")
+        }
+        posts.append(new_post)
+        save_posts(posts)
+        return redirect(url_for('index'))
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
